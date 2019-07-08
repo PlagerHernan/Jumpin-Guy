21c4e9ef09d646ce99175ac0c9637a874c86fdf5
Hernan <plagerhernan@gmail.com>
2019-07-08 11:12:39 -0300

    Nuevo repositorio inicializado

---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using UnityEngine.UI; //para acceder a objetos de interfaz 
+using System.Collections;
+
+public class GameController : MonoBehaviour {
+
+	[Range (0.02f, 0.3f)]
+	public float parallaxSpeed = 0.02f;
+	public RawImage background;
+	public RawImage platform;
+	public GameObject uiIdle;
+	public GameObject player;
+
+	public enum EstadoDelJuego {Parado, Jugando};
+	public EstadoDelJuego estadoDelJuego = EstadoDelJuego.Parado;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		//empieza el juego 
+		if (estadoDelJuego == EstadoDelJuego.Parado && Input.GetKey (KeyCode.Space)) {
+			estadoDelJuego = EstadoDelJuego.Jugando;
+			uiIdle.SetActive (false); //desactivo titulo e info
+			player.SendMessage ("PlayerState", "PlayerRun"); //envio mensaje a player
+		}
+
+		//juego en marcha
+		else if (estadoDelJuego == EstadoDelJuego.Jugando) {
+			Parallax ();
+		}
+	}
+
+	//movimiento pantalla
+	void Parallax(){
+		float finalSpeed = parallaxSpeed * Time.deltaTime; 
+		background.uvRect = new Rect (background.uvRect.x + finalSpeed, 0f, 1f, 1f); //Se mueve en x. El resto queda igual
+		platform.uvRect = new Rect (platform.uvRect.x + finalSpeed*4, 0f, 1f, 1f); //Se mueve 4 veces mas rapido que el background
+	}
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+
+	Animator _animator;
+
+	// Use this for initialization
+	void Start () {
+		_animator = GetComponent<Animator>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void PlayerState(string state = null)
+	{
+		if (state != null) {
+			_animator.Play(state);
+		}
+
+
+	} 
+}
